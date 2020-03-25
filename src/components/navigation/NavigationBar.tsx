b9036ba485dbf67424e81611a979b0ced7ab22c6
dimoonpl <dimoonpl@gmail.com>
2020-03-25 12:09:05 +0100

    replace hardcoded NavigationBar links

--- src/components/navigation/NavigationBar.tsx
+++ src/components/navigation/NavigationBar.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import reactLogo from 'img/react-logo.svg';
+import { ROUTES } from 'constants/routes';
 
 export const NavigationBar: React.FC = () => (
   <nav className='navigation-bar'>
-    <Link to='/'>
+    <Link to={ROUTES.INDEX}>
       <img className='navigation-bar__logo' src={reactLogo} alt=''/>
     </Link>
     <ul className='navigation-bar__menu'>
       <li className='navigation-bar__menu-item'>
-        <NavLink to='/articles'
+        <NavLink to={ROUTES.ARTICLES}
                  className='navigation-bar__link'
                  activeClassName='navigation-bar__link--active'>Articles</NavLink>
       </li>
